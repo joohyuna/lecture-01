e757b1e9d651a85ac15073def90fb579281a86ea
mcm <joohyuna2@gmail.com>
2026-04-03 15:21:42 +0900

    onChange, onSubmit 변수화

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,36 @@ function App() {
     // 이 인풋에서 일어나는 일은, 인풋에게 맡기자
     const [todo, setTodo] = useState("");
     const [list, setList] = useState([]);
+
+    const onChange = event => {
+        // console.log(event);
+        // 저 todo라는 state에 input에 입력받은 값을 지정시켜야 함
+        // event라고 하는, Javascript엔진이 분석한 사건 내용을 가지고 보니
+        // event.target.value 하는 값이 input에 입력된 값이 들어 있드라
+        // 덮어쓰기
+        setTodo(event.target.value);
+    }
+    const onSubmit = event => {
+        // chrome 값은 웹브라우저는 기본적으로, onSubmit이 내정되어 있는 기능이 미미 존재함
+        // 무슨 기능이냐면, input의 내용물을 전송하고 새로고침하는 기능
+        // 그래서 이 기능을 무효화 시킬 필요가 있음 ->  event.preventDefault();
+        // 새로고침은 이 페이지를 다시 불러오겠다는 것이기 때문에 무효화 해야 함 않그러면 이 페이지 내용이 몽땅 없어짐
+        event.preventDefault(); // 무료화 선언
+
+        if (todo === "") {
+            return;
+        }
+
+        // 1. todo에 저장되어 있는 값을 list로 옯기고
+        // list = [...list, "abc"] => 스프레드 문법(...) : 배열이나 객체의 내부요소를 나열시키는 문법
+        // list = [...["123"], "abc"]
+        // list = ["123", "abc"];
+        setList([...list, todo]);
+        // 2. todo의 값을 삭제하고
+        setTodo("");
+        // 3. input에 입력된 값도 삭제해야함 -> input이라고 하는 태그의 value 속성을 비워줘야 되는 일
+    }
+
     return (
         <div>
             <h1>My ToDo ({list.length})</h1>
@@ -16,26 +46,7 @@ function App() {
             form의 onSubmit  속성을 실행해줌
             */}
             <form
-                onSubmit={event => {
-                    // chrome 값은 웹브라우저는 기본적으로, onSubmit이 내정되어 있는 기능이 미미 존재함
-                    // 무슨 기능이냐면, input의 내용물을 전송하고 새로고침하는 기능
-                    // 그래서 이 기능을 무효화 시킬 필요가 있음 ->  event.preventDefault();
-                    // 새로고침은 이 페이지를 다시 불러오겠다는 것이기 때문에 무효화 해야 함 않그러면 이 페이지 내용이 몽땅 없어짐
-                    event.preventDefault(); // 무료화 선언
-
-                    if (todo === "") {
-                        return;
-                    }
-
-                    // 1. todo에 저장되어 있는 값을 list로 옯기고
-                    // list = [...list, "abc"] => 스프레드 문법(...) : 배열이나 객체의 내부요소를 나열시키는 문법
-                    // list = [...["123"], "abc"]
-                    // list = ["123", "abc"];
-                    setList([...list, todo]);
-                    // 2. todo의 값을 삭제하고
-                    setTodo("");
-                    // 3. input에 입력된 값도 삭제해야함 -> input이라고 하는 태그의 value 속성을 비워줘야 되는 일
-                }}>
+                onSubmit={onSubmit}>
                 {/*
                 input에 입력이 일어날 때마다 실행하는 속성 : onChange
                     입력이 일어난 "사건(이벤트)"이고,
@@ -44,14 +55,7 @@ function App() {
                  */}
                 <input
                     placeholder={"Write your to do..."}
-                    onChange={event => {
-                        // console.log(event);
-                        // 저 todo라는 state에 input에 입력받은 값을 지정시켜야 함
-                        // event라고 하는, Javascript엔진이 분석한 사건 내용을 가지고 보니
-                        // event.target.value 하는 값이 input에 입력된 값이 들어 있드라
-                        // 덮어쓰기
-                        setTodo(event.target.value);
-                    }}
+                    onChange={onChange}
                     value={todo}
                     // 1. input에서 엔터를 치면, onSubmit이 발동이 되고
                     // 2. ouSubmit안에 있는 setTodo를 실행시켜서 todo의 값을 "" (빈 스트링)으로 바꾸고
